4dfefe5c2e0d902e15a4c189af91e93cbe683b69
lichcat <lichcat@lichcat-laptop.(none)>
2012-07-29 22:48:50 +0800

    fvdl kind XML resolver using libxml2 lib

---
+++ fvdlResolve/src/fvdlParser.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <libxml/parser.h>
+#include <libxml/xpath.h>
+
+xmlDocPtr getFvdlDoc(char* docname){
+
+	xmlDocPtr doc;
+	xmlNodePtr cur;
+
+	doc=xmlParseFile(docname);
+	if(NULL==doc){
+		fprintf(stderr,"Document not parsed successfully. \n");
+		return NULL;
+	}
+	cur=xmlDocGetRootElement(doc);
+
+	if(NULL==cur){
+		fprintf(stderr,"empty document\n");
+		xmlFreeDoc(doc);
+		return NULL;
+	}
+	//printf("%d\n",xmlStrcmp(cur->name,(const xmlChar*)"FVDL"));	FVDL->0
+	if(xmlStrcmp(cur->name,(const xmlChar*)"FVDL")){
+		fprintf(stderr,"%s is not a FVDL file!\n",docname);
+		xmlFreeDoc(doc);
+		return NULL;
+	}
+	
+	return doc;
+}
+
+xmlXPathObjectPtr getnodeset(xmlDocPtr doc, xmlChar *xpath){
+	int status;
+	xmlXPathContextPtr context;
+	xmlXPathObjectPtr result;
+
+	context = xmlXPathNewContext(doc);
+	status = xmlXPathRegisterNs(context,(const xmlChar*)"fvdl",
+			(const xmlChar*)"xmlns://www.fortifysoftware.com/schema/fvdl");
+	if(status!=0){
+		fprintf(stderr,"XML Namespace register fail\n");
+		xmlXPathFreeContext(context);
+		return NULL;
+	}
+	result = xmlXPathEvalExpression(xpath,context);
+	if(xmlXPathNodeSetIsEmpty(result->nodesetval)){
+		printf("No result\n");
+		xmlXPathFreeContext(context);
+		return NULL;
+	}
+	xmlXPathFreeContext(context);
+	return result;
+
+}
+
+int main(int argc, char** argv){
+	int i;
+	char* fvdlDocName;
+	xmlDocPtr doc;
+	xmlChar *xpath="//fvdl:Vulnerability";
+	xmlNodeSetPtr nodeset;
+	xmlXPathObjectPtr result;
+
+	xmlChar * keyword;
+
+	if(argc <=1){
+		printf("Usage: %s docname\n",argv[0]);
+		return (0);
+	}
+	fvdlDocName=argv[1];
+	doc=getFvdlDoc(fvdlDocName);
+
+	if(NULL!=doc)
+		result=getnodeset(doc,xpath);
+	else
+		return (0)
+			;
+	if(result){
+		nodeset = result->nodesetval;
+		for(i=0;i< nodeset->nodeNr; i++){
+			keyword = xmlNodeListGetString(doc, nodeset->nodeTab[i]->xmlChildrenNode,1);
+			printf("keyword: %s \n",keyword);
+			xmlFree(keyword);
+		}
+		xmlXPathFreeObject(result);
+	}
+	xmlFreeDoc(doc);
+	xmlCleanupParser();
+	return(1);
+
+}

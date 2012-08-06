92e82a7aa9f9ffe9ffed10c8f8e2d96d15077040
lichcat <mengchenli.nju@gmail.com>
2012-08-06 13:24:47 +0800

    prepare for reachability computation dumpCFG for print cfg stored and debugging

--- EnhancedCrestInstru/src/process_cfg/process_cfg.cc
+++ EnhancedCrestInstru/src/process_cfg/process_cfg.cc
@@ -19,13 +19,25 @@
 #include <vector>
 #include <ext/hash_map>
 
+#include <regex.h>
+//for print debug
+//#include <iostream>
+#include <assert.h>
+
+#ifdef DEBUG
+#define IFDEBUG(x) x
+#else
+#define IFDEBUG(x)
+#endif
+
+
 using namespace std;
 using __gnu_cxx::hash_map;
 
-typedef pair<int,int> edge_t;
-typedef vector< pair<int,size_t> > adj_list_t;
-typedef adj_list_t::iterator nbhr_it;
-typedef adj_list_t::const_iterator const_nbhr_it;
+//typedef pair<int,int> edge_t;
+typedef vector<int> adj_list_t;
+//typedef adj_list_t::iterator nbhr_it;
+//typedef adj_list_t::const_iterator const_nbhr_it;
 typedef vector<adj_list_t> graph_t;
 typedef set<int>::const_iterator BranchIt;
 
@@ -52,7 +64,28 @@ void readBranches(set<int>* branches) {
 
 	in.close();
 }
+bool isPathMark(string funcname){
+	regex_t regex;
+	int reti;
+	char msgbuf[100];
+	//reti = regcomp(&regex,"__StaticPathMark_P[0-9]+_S[0-9]+",0);
+	reti = regcomp(&regex,"__StaticPathMark.*",0);
+	if(reti){
+		fprintf(stderr,"could not compile regex \n");
+		exit(1);
+	}
 
+	reti=regexec(&regex,funcname.c_str(),0,NULL,0);
+	if(!reti)
+		return true;
+	else if(reti == REG_NOMATCH)
+		return false;
+	else{
+		regerror(reti,&regex,msgbuf,sizeof(msgbuf));
+		fprintf(stderr,"Regex match failed : %s\n",msgbuf);
+		exit(1);
+	}
+}
 void readCfg(graph_t* graph) {
 	// First we have to read in the function -> CFG node map.
 	hash_map<string,int> funcNodeMap;
@@ -70,11 +103,13 @@ void readCfg(graph_t* graph) {
 	ifstream in("cfg");
 
 	string line;
+	int pathLen=0;		// count staticpathstmt number
 	while (getline(in, line)) {
 		istringstream line_in(line);
 		int src;
 		line_in >> src;
 		line_in.get();
+		assert(src>0);
 
 		if (graph->size() <= src)
 			graph->resize(src+1);
@@ -84,65 +119,41 @@ void readCfg(graph_t* graph) {
 			if (isdigit(line_in.peek())) {
 				int dst;
 				line_in >> dst;
-				nbhrs.push_back(make_pair(dst, 0));
+				nbhrs.push_back(dst);
 			} else {
 				string func;
 				line_in >> func;
 				hash_map<string,int>::iterator it = funcNodeMap.find(func);
 				if (it != funcNodeMap.end())
-					nbhrs.push_back(make_pair(it->second,0));
-				//else if () match __StaticPathMark_P[digit]+S[digit]+
+					nbhrs.push_back(it->second);
+				else if (isPathMark(func)){
+					pathLen++;
+					string pNsN,stmtStr;
+					pNsN=func.substr(16);
+					size_t stmtIdPos=pNsN.find_last_of("S");
+					stmtStr = pNsN.substr(stmtIdPos+1);
+					int stmtValue=atoi(stmtStr.c_str());
+					//cout<<"match "<<(-stmtValue)<<endl;
+					nbhrs.push_back(-stmtValue);
+				}
 			}
 			line_in.get();
 		}
 	}
-
+	//cout<<"pathLen "<<pathLen<<endl;
 	in.close();
 }
-
-void dijkstra_bounded_shortest_paths
-(const graph_t& g, int src, vector<size_t>& dist_map, size_t max_dist) {
-
-	// Initialize all distances.
-	for (int i = 0; i < g.size(); i++) {
-		dist_map[i] = numeric_limits<size_t>::max();
-	}
-	dist_map[src] = 0;
-
-	// Initialize the queue.
-	set< pair<size_t, int> > Q;
-	Q.insert(make_pair(0, src));
-
-	// Initialize the sets of seen and finished vertices.
-	set<int> finished;
-
-	while (!Q.empty()) {
-		// Pop the head of the queue.
-		int v = Q.begin()->second;
-		size_t dist = Q.begin()->first;
-		Q.erase(Q.begin());
-
-		// If the current distance is greater than the max, we're done.
-		if (dist > max_dist)
-			break;
-
-		// Otherwise, process the vertex.
-		finished.insert(v);
-		for (const_nbhr_it e = g[v].begin(); e != g[v].end(); ++e) {
-			int u = e->first;
-			if (finished.find(u) == finished.end()) {
-				size_t d = dist_map[v] + e->second;
-				if ((d < dist_map[u]) && (d <= max_dist)) {
-					Q.erase(make_pair(dist_map[u], u));
-					Q.insert(make_pair(d, u));
-					dist_map[u] = d;
-				}
-			}
-		}
+void dumpCFG(graph_t *graph){
+	//printf("size: %d\n",graph->size());
+	unsigned int i,j;
+	for(i=0;i<graph->size();i++){
+		fprintf(stderr,"%d",i);
+		adj_list_t &nbhrs =  (*graph)[i];
+		for(j=0;j<nbhrs.size();j++)
+			fprintf(stderr," %d",nbhrs[j]);
+		fprintf(stderr,"\n");	
 	}
 }
-
-
 int main(void) {
 
 	// Read in the set of branches.
@@ -155,52 +166,7 @@ int main(void) {
 	cfg.reserve(1000000);
 	readCfg(&cfg);
 	fprintf(stderr, "Read %d nodes.\n", cfg.size());
+	//IFDEBUG(dumpCFG(&cfg);)
 
-	// Set the length of every edge to 1 if the destination is a branch,
-	// and zero otherwise.
-	for (size_t i = 0; i < cfg.size(); i++) {
-		for (nbhr_it j = cfg[i].begin(); j != cfg[i].end(); ++j) {
-			if (branches.find(j->first) != branches.end()) {
-				j->second = 1;
-			} else {
-				j->second = 0;
-			}
-		}
-	}
-
-	std::ofstream out("cfg_branches", std::ios::out | std::ios::binary);
-	size_t len = branches.size();
-	out.write((char*)&len, sizeof(len));
-
-	// "Thin" the graph down to unit-length edges between branches by, for each
-	// branch, running Dijstrak's Algorithm until all other branches distance
-	// one away have been discovered.  We print out an adjacency list for the
-	// thinned graph as we go.
-	vector<size_t> dist(cfg.size());
-	vector<int> nbhrs;
-	int numEdges = 0;
-	for (BranchIt i = branches.begin(); i != branches.end(); ++i) {
-		// Compute all shortest-paths of length zero and one from vertex i.
-		dijkstra_bounded_shortest_paths(cfg, *i, dist, 1);
-
-		// Accumulate the branch nodes distance one from i.
-		nbhrs.clear();
-		for (BranchIt j = branches.begin(); j != branches.end(); ++j) {
-			if (dist[*j] == 1) {
-				nbhrs.push_back(*j);
-			}
-		}
-		numEdges += nbhrs.size();
-
-		// Write out the neighbors.
-		len = nbhrs.size();
-		int dest = *i;
-		out.write((char*)&dest, sizeof(dest));
-		out.write((char*)&len, sizeof(len));
-		out.write((char*)&nbhrs.front(), len * sizeof(int));
-	}
-
-	out.close();
-	fprintf(stderr, "Wrote %d branch edges.\n", numEdges);
 	return 0;
 }
